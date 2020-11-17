57243c5e13bf61edf372139ade3b7d1653429718
Hoon Kim <b0aeiou@gmail.com>
2020-11-17 20:00:48 +0900

    수정 : step6/Q4673.java 내용 : 주석 추가

--- step6/Q4673.java
+++ step6/Q4673.java
@@ -3,12 +3,15 @@ package baekjoon.step6;
 public class Q4673 {
 	public static void main(String[] args) {
 
+		// 생성자가 존재하는지 체크할 boolean 배열 flag 생성
 		boolean[] flag = new boolean[10000];
 
 		for (int i = 0; i < 10000; i++){
 			
+			// 1부터 10000까지 d(n) 함수의 연산결과를 변수 n으로 생성
 			int n = d(i+1);
 			
+			// n이 10000 이하라면, flag의 n-1번째 인덱스를 true로 초기화
 			if(n < 10001){
 				flag[n-1] = true;
 			}
@@ -18,6 +21,7 @@ public class Q4673 {
 
 		for (int i = 0; i < 10000; i++) {
 			
+			// flag 값이 true인 인덱스를 기준으로 i+1을 출력
 			if (!flag[i]) {
 				sb.append(i+1).append('\n');
 			}
@@ -26,13 +30,14 @@ public class Q4673 {
 		System.out.println(sb);
 	}
 
-	static int d(int number){
+	// 양의 정수 n에 대해서 d(n)을 n과 n의 각 자리수를 더하는 함수
+	static int d(int n){
 		
-		int sum = number;
+		int sum = n;
 		
-		while(number != 0){
-			sum += (number % 10);
-			number = number/10;
+		while(n != 0){
+			sum += (n % 10);
+			n = n/10;
 		}
 		
 		return sum;
