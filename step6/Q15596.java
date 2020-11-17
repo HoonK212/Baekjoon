5d55b35b1f18b46d3685880d7ae3468b4e7d3309
Hoon Kim <b0aeiou@gmail.com>
2020-11-17 19:41:46 +0900

    수정 : step6/Q4673.java, step6/Q15596.java

--- step6/Q15596.java
+++ step6/Q15596.java
@@ -1,8 +1,20 @@
 package baekjoon.step6;
 
 public class Q15596 {
-	long sum(int[] arr) {
+	public static void main(String[] args) {
+		
+		int[] a = {1, 2, 3, 4, 5};
+		System.out.println(sum(a));
+	}
+	
+	static long sum(int[] a) {
+
+		int len = a.length;
 		long res = 0;
+		
+		for(int i=0; i<len; i++) {
+			res += a[i];
+		}
 		return res;
 	}
 }
--- step6/Q4673.java
+++ step6/Q4673.java
@@ -1,12 +1,40 @@
 package baekjoon.step6;
 
 public class Q4673 {
-	int d(int number){
+	public static void main(String[] args) {
+
+		boolean[] flag = new boolean[10000];
+
+		for (int i = 0; i < 10000; i++){
+			
+			int n = d(i+1);
+			
+			if(n < 10001){
+				flag[n-1] = true;
+			}
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < 10000; i++) {
+			
+			if (!flag[i]) {
+				sb.append(i+1).append('\n');
+			}
+		}
+		
+		System.out.println(sb);
+	}
+
+	static int d(int number){
+		
 		int sum = number;
+		
 		while(number != 0){
-			sum = sum + (number % 10);
+			sum += (number % 10);
 			number = number/10;
 		}
+		
 		return sum;
 	}
 }
