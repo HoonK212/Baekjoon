72cce9e0ae3c6b61b45d83e6d8bf590699b6ba50
Hoon Kim <b0aeiou@gmail.com>
2020-12-08 23:42:31 +0900

    수정 : step7/Q10809.java, step7/Q2675.java 내용 : 주석 추가

--- step7/Q10809.java
+++ step7/Q10809.java
@@ -4,24 +4,28 @@ import java.util.Scanner;
 
 public class Q10809 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
- 
-		int[] arr = new int[26];
 		
+		// 단어를 입력받아 String 변수로 생성
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+ 
+		// int 배열을 생성하여 각각의 알파벳이 단어에 포함되어 있지 않은 경우부터 완성
+		int[] arr = new int[26];
 		for(int i = 0; i < arr.length; i++) {
 			arr[i] = -1;
 		}
  
-		String str = sc.nextLine();
- 
+		// 각 알파벳의 위치를 for문의 i값을 활용하여 초기화
+		char ch;
 		for(int i = 0; i < str.length(); i++) {
-			char ch = str.charAt(i);
+			ch = str.charAt(i);
     
 			if(arr[ch - 'a'] == -1) {
 				arr[ch - 'a'] = i;
 			}
 		}
  
+		// for each문으로 결과값 출력
 		for(int res : arr) {
 			System.out.print(res + " ");
 		}
--- step7/Q2675.java
+++ step7/Q2675.java
@@ -10,21 +10,26 @@ public class Q2675 {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
         
-		int t = Integer.parseInt(in.readLine());  
+		// 테스트 케이스의 개수 T
+		int t = Integer.parseInt(in.readLine());
+		
+		// 문자를 반복할 횟수 R
 		int r;
  
 		for (int i = 0; i < t; i++) {
  
+			// str[0] -> 반복할 횟수 R, str[1] -> 반복할 문자열 S
 			String[] str = in.readLine().split(" ");
- 
 			r = Integer.parseInt(str[0]);
  
+			// getBytes() 메소드를 활용하여 문자열을 byte 배열로 변환한다.
 			for (byte val : str[1].getBytes()) {
 				for (int j = 0; j < r; j++) {
 					sb.append((char)val);
 				}
 			}
 			
+			// 각 테스트 케이스마다 개행
 			sb.append('\n');
 		}
 		
