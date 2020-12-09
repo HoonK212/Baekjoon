ad2253353e99bf0f0773b4c0761ceba5849a99c1
Hoon Kim <b0aeiou@gmail.com>
2020-12-09 22:52:11 +0900

    추가 : step7/Q2908.java 내용 : 단계별 문제 풀이 진행

---
+++ step7/Q2908.java
@@ -0,0 +1,19 @@
+package baekjoon.step7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q2908 {
+	public static void main(String[] args) throws IOException {
+		 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+		int a = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		int b = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		
+		System.out.print(a > b ? a:b);
+	}
+}
